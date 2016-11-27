2104afccf2d53b9a14ecc0435785a65c64a6aeec
CyanideAndHappiness <karina__96@list.ru>
2016-11-28 00:31:51 +0300

    Создан новый проект

---
+++ NewApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. This is my first program on GitHub");
+            Console.ReadLine();
+        }
+    }
+}
